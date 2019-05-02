96e75833ba6c0f2528a57b6a55eb7458050ceea4
April Kim <dkim24@risd.edu>
2019-05-02 12:46:38 -0400

    pr3 retry

--- project3/project3.js
+++ project3/project3.js
@@ -1,3 +1,17 @@
+//Set the target date & time
+var countUpDate = new Date ("May 2, 2019 12:44:00").getTime();
+
+//Get today's date & time
+var now = new Date().getTime();
+
+//Find the distance between now and the target date & time
+var distance = countUpDate - now {
+	document.getElementById("waterLevel").setAttribute("style", "height: 100%");
+}
+
+//Display the result in an element with id="waterLevel"
+
+/**
 var d, s, sheight;
 function adjustWaterLevel() {
 	d = new Date();
@@ -8,4 +22,4 @@ function adjustWaterLevel() {
 
 window.addEventListener("load", function(){
 	setInterval( adjustWaterLevel, 1000);
-} );
+} );**/
